96c454b390bdc636e23e49a1a4cf292b9d177ef3
Ansh-coder-Ng <a.palekar@ancrewglobal.com>
2025-12-08 14:02:34 +0530

    Addedd Css

--- frontend/src/pages/TeacherMain.jsx
+++ frontend/src/pages/TeacherMain.jsx
@@ -1,83 +1,91 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import styles from "../css/TeacherMain.module.css";
 
 export function TeacherMain() {
   const [open1, setOpen1] = useState(false);
   const [open2, setOpen2] = useState(false);
-  const [classTeacherData,setCt]=useState([])
-  const [eventTeacherData,setEt]=useState([])
+  const [classTeacherData, setCt] = useState([]);
+  const [eventTeacherData, setEt] = useState([]);
 
   useEffect(() => {
-    const teacher_name=localStorage.getItem("teacher_name")
-    const fetchForClassTeacher=async()=>{
-          const res=await axios.get(`http://15.206.143.199:8000/fetchForClassTeacher/?teacher_name=${teacher_name}`)
-          print(res.data)
-          setCt(res.data.student_list)
+    const teacher_name = localStorage.getItem("teacher_name");
+
+    const fetchForClassTeacher = async () => {
+      const res = await axios.get(
+        `http://15.206.143.199:8000/fetchForClassTeacher/?teacher_name=${teacher_name}`
+      );
+      console.log(res.data);
+      setCt(res.data.student_list);
+    };
+
+    const fetchForEventTeacher = async () => {
+      const res = await axios.get(
+        `http://15.206.143.199:8000/fetchForEventTeacher/?teacher_name=${teacher_name}`
+      );
+      setEt(res.data.student_list);
+    };
+
+    fetchForClassTeacher();
+    fetchForEventTeacher();
+  }, []);
+
+  const markAttendance = async (student_name) => {
+    const res = await axios.post(
+      `http://15.206.143.199:8000/markAttendance/`,
+      { student_name }
+    );
+    return res.data.success === true;
+  };
+
+  const approveAttendance = async (student_name) => {
+    const res = await axios.post(
+      `http://15.206.143.199:8000/approveAttendance/`,
+      { student_name }
+    );
+    return res.data.success === true;
+  };
+
+  const deleteRow = async (student_name) => {
+    const res = await markAttendance(student_name);
+    if (res) {
+      setCt((prev) => prev.filter((i) => i !== student_name));
     }
+  };
 
-    const fetchForEventTeacher=async()=>{
-        const res=await axios.get(`http://15.206.143.199:8000/fetchForEventTeacher/?teacher_name=${teacher_name}`)
-        setEt(res.data.student_list)
+  const deleteRow1 = async (student_name) => {
+    const res = await approveAttendance(student_name);
+    if (res) {
+      setEt((prev) => prev.filter((i) => i !== student_name));
     }
-
-    fetchForClassTeacher()
-    fetchForEventTeacher()
-
-    console.log(classTeacherData)
-  }, [])
-
-
-  const markAttendance=async(student_name)=>{
-      const res=await axios.post(`http://15.206.143.199:8000/markAttendance/`,{student_name:student_name})
-      if(res.data.success === true){
-        return true
-      }
-      return false
-  }
-
-  const approveAttendance=async(student_name)=>{
-      const res=await axios.post(`http://15.206.143.199:8000/approveAttendance/`,{student_name:student_name})
-      if(res.data.success === true){
-        return true
-      }
-      return false
-  }
-
-
-  const deleteRow=async(student_name)=>{
-      const res=markAttendance(student_name)
-      if(res){
-        setCt(prev => prev.filter(i => i !== student_name));
-      }
-  }
-
-  const deleteRow1=async(student_name)=>{
-      const res=approveAttendance(student_name)
-      if(res){
-        setEt(prev => prev.filter(i => i !== student_name));
-      }
-  }
+  };
 
   return (
-    <div className="container">
-      {/* DROPDOWN 1 */}
-      <div className="dropdownWrapper">
+    <div className={styles.container}>
+      
+      {/* CLASS TEACHER DROPDOWN */}
+      <div className={styles.dropdownWrapper}>
         <button
-          className="dropdownButton"
+          className={styles.dropdownButton}
           onClick={() => setOpen1(!open1)}
         >
           Fetch For Class Teacher
         </button>
 
         {open1 && (
-          <div className="dropdownList">
-            <table className="table">
+          <div className={styles.dropdownList}>
+            <table className={styles.table}>
               <tbody>
-                {classTeacherData.map((item,index) => (
+                {classTeacherData.map((item, index) => (
                   <tr key={index}>
                     <td>{item}</td>
                     <td>
-                      <button className="approveButton" onClick={()=>deleteRow(item)}>Approve</button>
+                      <button
+                        className={styles.approveButton}
+                        onClick={() => deleteRow(item)}
+                      >
+                        Approve
+                      </button>
                     </td>
                   </tr>
                 ))}
@@ -87,24 +95,29 @@ export function TeacherMain() {
         )}
       </div>
 
-      {/* DROPDOWN 2 */}
-      <div className="dropdownWrapper">
+      {/* EVENT TEACHER DROPDOWN */}
+      <div className={styles.dropdownWrapper}>
         <button
-          className="dropdownButton"
+          className={styles.dropdownButton}
           onClick={() => setOpen2(!open2)}
         >
           Fetch For Event Teacher
         </button>
 
         {open2 && (
-          <div className="dropdownList">
-            <table className="table">
+          <div className={styles.dropdownList}>
+            <table className={styles.table}>
               <tbody>
-                {eventTeacherData.map((item,index) => (
+                {eventTeacherData.map((item, index) => (
                   <tr key={index}>
                     <td>{item}</td>
                     <td>
-                      <button className="approveButton" onClick={()=>deleteRow1(item)}>Approve</button>
+                      <button
+                        className={styles.approveButton}
+                        onClick={() => deleteRow1(item)}
+                      >
+                        Approve
+                      </button>
                     </td>
                   </tr>
                 ))}
@@ -113,6 +126,7 @@ export function TeacherMain() {
           </div>
         )}
       </div>
+
     </div>
   );
 }
