7e73a1f380baddb9f1f1875be0c5d7c368049da4
Ansh-coder-Ng <a.palekar@ancrewglobal.com>
2025-12-10 14:17:20 +0530

    Changed ALB DOMAIN

--- frontend/src/pages/StudentMain.jsx
+++ frontend/src/pages/StudentMain.jsx
@@ -26,7 +26,7 @@ export const StudentMain=()=>{
 
            try {
             const response = await axios.post(
-                "http://my-alb-20076270.ap-south-1.elb.amazonaws.com/submitForm/",
+                "http://my-load-balancer-722992164.ap-south-1.elb.amazonaws.com/submitForm/",
                 formData,
                 {
                     headers: {
